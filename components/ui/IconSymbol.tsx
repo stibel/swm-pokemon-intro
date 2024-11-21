7ad1744fc845b34a4912dd17f5afeb6be300a3cc
Mikołaj Siebielec <stibel@MacBook-Pro-Mikoaj.local>
2024-11-21 14:12:31 +0100

    fix(misc): add mappings for android icons

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -9,10 +9,12 @@ import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
+  'heart.fill': 'favorite',
+  'list.bullet.rectangle.portrait.fill': 'list',
+  'camera.fill': 'photo-camera',
+  'map.fill': 'map',
+  'xmark.circle.fill': 'cancel',
+  'heart.circle.fill': 'favorite'
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
