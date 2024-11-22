9da0c54d9229681815637f6f0bebaa112f896b3c
Mikołaj Siebielec <stibel@MBP-Mikoaj.NAT.warszawa.vectranet.pl>
2024-11-22 11:47:19 +0100

    feat(camera): wip

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,38 +1,25 @@
 import { Image, StyleSheet } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function ProfileScreen() {
+  const device = useCameraDevice('front')
+  const { hasPermission, requestPermission } = useCameraPermission()
+
+  if (!hasPermission) {
+    requestPermission();
+  }
+  if (device == null) return <ThemedText>no deviceno deviceno deviceno deviceno deviceno device</ThemedText>
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedText>camera</ThemedText>
-    </ParallaxScrollView>
+    <ThemedView style={{ flex: 1 }}>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+      />
+    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
