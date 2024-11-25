6823a33fa07b9e601b58bbfd961e4be0ea717858
Mikołaj Siebielec <stibel@MacBook-Pro-Mikoaj.local>
2024-11-25 11:25:05 +0100

    feat(camera): add working front camera view

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -4,7 +4,11 @@ import { Camera, useCameraDevice, useCameraPermission } from 'react-native-visio
 
 export default function CameraScreen() {
   const device = useCameraDevice('front')
-  const { hasPermission } = useCameraPermission()
+  const { hasPermission, requestPermission } = useCameraPermission()
+
+  if (!hasPermission) {
+    requestPermission();
+  }
 
   if (!hasPermission) return <ThemedText> no permission </ThemedText>
   if (device == null) return <ThemedText> no device </ThemedText>
