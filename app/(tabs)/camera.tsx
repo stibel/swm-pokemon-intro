9fdec31913f59b67845036ddef339aedc985fef0
Mikołaj Siebielec <stibel@MacBook-Pro-Mikoaj.local>
2024-11-25 14:19:36 +0100

    feat(camera): add config and simple frame processor

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,6 +1,12 @@
 import { StyleSheet } from 'react-native';
+import {
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+
+import { Camera } from 'react-native-vision-camera';
 import { ThemedText } from '@/components/ThemedText';
-import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function CameraScreen() {
   const device = useCameraDevice('front')
@@ -10,10 +16,17 @@ export default function CameraScreen() {
     requestPermission();
   }
 
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+    console.log(frame)
+  }, [])
+
   if (!hasPermission) return <ThemedText> no permission </ThemedText>
   if (device == null) return <ThemedText> no device </ThemedText>
+
   return (
     <Camera
+      frameProcessor={frameProcessor}
       style={StyleSheet.absoluteFill}
       device={device}
       isActive={true}
---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        [
+            'react-native-worklets-core/plugin',
+        ],
+        [
+            'react-native-reanimated/plugin',
+        ],
+    ],
+};
