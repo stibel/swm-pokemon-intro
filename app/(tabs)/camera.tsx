4140710719be949c7bbc7bfcb62b0d37e151e006
Mikołaj Siebielec <stibel@MacBook-Pro-Mikoaj.local>
2024-11-25 11:04:46 +0100

    feat(camera): install react native camera package

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,38 +1,18 @@
-import { Image, StyleSheet } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
-export default function ProfileScreen() {
+export default function CameraScreen() {
+  const device = useCameraDevice('front')
+  const { hasPermission } = useCameraPermission()
+
+  if (!hasPermission) return <ThemedText> no permission </ThemedText>
+  if (device == null) return <ThemedText> no device </ThemedText>
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedText>camera</ThemedText>
-    </ParallaxScrollView>
-  );
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
+  )
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
